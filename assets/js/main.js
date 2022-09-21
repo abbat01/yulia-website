05ac5b101b7f9970ce529bc68d0c8a7483cbfc58
Andrey Abaturov <95858072+abbat01@users.noreply.github.com>
2022-09-21 20:16:49 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,34 @@ const swiper = new Swiper('.swiper', {
   
     // And if we need scrollbar
   });
+
+  const select = document.querySelector('select');
+  const allLang = ['ua', 'ru']
+
+  select.addEventListener('change', changeURLLang);
+
+  function changeURLLang() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+  }
+
+  function changeLang() {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    console.log(hash)
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#ru';
+        location.reload();
+    }
+    select.value = hash
+
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key)
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+  }
+
+  changeLang()
